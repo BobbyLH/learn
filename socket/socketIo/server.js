2b9a84af2e209a0736aa7ddd99c5b45b73bcffc7
bobby.li <bobby.li@ximalaya.com>
2018-12-10 15:00:25 +0800

    socket io

--- socket/socketIo/server.js
+++ socket/socketIo/server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const io = require('socket.io')
 const app = express()
+const server = require('http').createServer(app)
+const ioServer = io(server)
 
 app.use(express.static(__dirname))
 
-app.listen(3001)
+ioServer.on('connection', socket => {
+  socket.send('[Server Msg - connection]: Merry Christmas')
+  socket.on('message', msg => {
+    console.log('[Server Msg From Client - message]:', msg)
+    socket.send('[Server Msg To Send - message]: Happy New Year')
+  })
+})
 
+server.listen(3001)
